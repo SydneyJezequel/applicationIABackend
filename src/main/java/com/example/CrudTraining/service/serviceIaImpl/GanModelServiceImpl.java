60fe4f12eee8f6ac512fdb93ef859fd63b6d1884
Sydney Jezequel <107682562+SydneyJezequel@users.noreply.github.com>
2024-01-30 23:13:25 +0100

    Commit 24 : Mise à jour de la fonctionnalité qui entraine le Modèle de Génération d'image GAN.

--- src/main/java/com/example/CrudTraining/service/serviceIaImpl/GanModelServiceImpl.java
+++ src/main/java/com/example/CrudTraining/service/serviceIaImpl/GanModelServiceImpl.java
@@ -27,8 +27,8 @@ public class GanModelServiceImpl implements GanModelService {
 
 
     // *************************** Urls pour manipuler le modèle Gan *************************** //
-    private final String urlGenerateGanImage = "http://localhost:8008/generate-faces";
-    private final String urlTrainGanImage = "http://localhost:8008/train-gan-model";
+    private final String urlGenerateGanImage = "http://localhost:8009/generate-faces";
+    private final String urlTrainGanImage = "http://localhost:8009/train-gan-model";
 
 
 
@@ -70,57 +70,60 @@ public class GanModelServiceImpl implements GanModelService {
     }
 
 
-        /**
-         * Méthode pour générer des images avec le modèle GAN.
-         *
-         */
-        @Override
-        public boolean trainGanModel (int nbEpochs, int batchSize, double lr, int zDim, String device, int showStep, int saveStep) {
-            try {
-                // ********************** TEST ********************** //
-                nbEpochs = 10000;
-                batchSize = 128;
-                lr = 1e-4;
-                zDim = 200;
-                device = "cpu";
-                showStep = 35;
-                saveStep = 35;
-                // ********************** TEST ********************** //
-                // Création de l'en-tête de la requête Http :
-                HttpHeaders headers = new HttpHeaders();
-                headers.setContentType(MediaType.APPLICATION_JSON);
-                // Création du contenu de la requête Http :
-                Map<String, Object> requestBodyMap = new HashMap<>();
-                requestBodyMap.put("nb_epochs", nbEpochs);
-                requestBodyMap.put("batch_size", batchSize);
-                requestBodyMap.put("lr", lr);
-                requestBodyMap.put("z_dim", zDim);
-                requestBodyMap.put("device", device);
-                requestBodyMap.put("show_step", showStep);
-                requestBodyMap.put("save_step", saveStep);
+    /**
+     * Méthode pour générer des images avec le modèle GAN.
+     *
+     */
+    @Override
+    public boolean trainGanModel (int nbEpochs, int batchSize, double lr, int zDim, String device, int showStep, int saveStep) {
+        try {
+            // ********************** TEST ********************** //
+            /*
+            nbEpochs = 10000;
+            batchSize = 128;
+            lr = 1e-4;
+            zDim = 200;
+            device = "cpu";
+            showStep = 35;
+            saveStep = 35;
+            */
+            // ********************** TEST ********************** //
+            // Création de l'en-tête de la requête Http :
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            // Création du contenu de la requête Http :
+            Map<String, Object> requestBodyMap = new HashMap<>();
+            requestBodyMap.put("n_epochs", nbEpochs);
+            requestBodyMap.put("batch_size", batchSize);
+            requestBodyMap.put("lr", lr);
+            requestBodyMap.put("z_dim", zDim);
+            requestBodyMap.put("device", device);
+            requestBodyMap.put("show_step", showStep);
+            requestBodyMap.put("save_step", saveStep);
 
-                // Convertir la map en JSON :
-                String requestBody = new ObjectMapper().writeValueAsString(requestBodyMap);
-                HttpEntity<String> httpEntity = new HttpEntity<>(requestBody, headers);
-                // Exécution de la requête vers l'API du modèle de Machine Learning :
-                RestTemplate restTemplate = new RestTemplate();
-                ResponseEntity<String> responseEntity = restTemplate.exchange(
-                            urlTrainGanImage,
-                            HttpMethod.POST,
-                            httpEntity,
-                            String.class
-                    );
-                    // Récupération et renvoie de la réponse :
-                    logger.info(responseEntity.getBody());
-                    return true;
-                }catch (RuntimeException e){
-                    logger.info(e.toString());
-                    return false;
-                } catch (JsonProcessingException e) {
-                    logger.info(e.toString());
-                    return false;
-                }
-            }
+            // Convertir la map en JSON :
+            ObjectMapper objectMapper = new ObjectMapper();
+            String requestBody = objectMapper.writeValueAsString(requestBodyMap);
+            HttpEntity<String> httpEntity = new HttpEntity<>(requestBody, headers);
+            // Exécution de la requête vers l'API du modèle de Machine Learning :
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> responseEntity = restTemplate.exchange(
+                    urlTrainGanImage,
+                    HttpMethod.POST,
+                    httpEntity,
+                    String.class
+            );
+            // Récupération et renvoie de la réponse :
+            logger.info(responseEntity.getBody());
+            return true;
+        }catch (RuntimeException e){
+            logger.info(e.toString());
+            return false;
+        } catch (JsonProcessingException e) {
+            logger.info(e.toString());
+            return false;
+        }
+    }
 
 
 
